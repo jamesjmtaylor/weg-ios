7c0739815916c41ef950038837dfaf2efaad91da
James Taylor <james.jm.taylor@gmail.com>
2018-04-23 18:06:08 -0700

    initial commit

---
+++ Podfile
@@ -0,0 +1,9 @@
+
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '9.0'
+use_frameworks!
+
+target 'weg-ios' do
+  pod 'Kingfisher', '~> 4.7.0'
+  pod 'lottie-ios'
+end
---
+++ weg-ios/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  weg-ios
+//
+//  Created by Taylor, James on 4/23/18.
+//  Copyright © 2018 James JM Taylor. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
