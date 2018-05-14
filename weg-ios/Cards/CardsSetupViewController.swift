fc9d0bf937ffe281ae9c09982bb817720f9c6935
James Taylor <james.jm.taylor@gmail.com>
2018-05-14 05:43:55 -0700

    Added UI for flashcards

---
+++ weg-ios/Cards/CardsSetupViewController.swift
@@ -0,0 +1,27 @@
+//
+//  CardsSetupViewController.swift
+//  weg-ios
+//
+//  Created by Taylor, James on 5/14/18.
+//  Copyright © 2018 James JM Taylor. All rights reserved.
+//
+
+import UIKit
+
+class CardsSetupViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+    }
+
+
+}
