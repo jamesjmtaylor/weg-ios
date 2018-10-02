3b74b7da9f9f7f5b8afb829e78377074a0615dc2
James Taylor <james.jm.taylor@gmail.com>
2018-10-02 03:04:32 +0200

    Added mockingjay

--- Podfile
+++ Podfile
@@ -1,21 +1,22 @@
-
-source 'https://github.com/CocoaPods/Specs.git'
 platform :ios, '9.0'
 use_frameworks!
 
-target 'weg-ios' do
+def main_pods
   pod 'Firebase/Core'
   pod 'Kingfisher', '~> 4.7.0'
   pod 'lottie-ios'
   pod 'Fabric', '~> 1.7.7'
   pod 'Crashlytics', '~> 3.10.2'
+end
 
-  target 'wegUnitTests' do
-        inherit! :search_paths
-        pod 'Firebase/Core'
-        pod 'Kingfisher', '~> 4.7.0'
-        pod 'lottie-ios'
-        pod 'Fabric', '~> 1.7.7'
-        pod 'Crashlytics', '~> 3.10.2'
-    end
+
+target 'weg-ios' do
+  main_pods
+end
+
+target 'wegUnitTests' do
+  use_frameworks!
+  inherit! :search_paths
+  pod 'Mockingjay'
+  main_pods
 end
---
+++ wegUnitTests/repoUnitTests.swift
@@ -0,0 +1,31 @@
+//
+//  repoUnitTests.swift
+//  wegUnitTests
+//
+//  Created by Taylor, James on 10/2/18.
+//  Copyright © 2018 James JM Taylor. All rights reserved.
+//
+
+import XCTest
+import Mockingjay
+
+class repoUnitTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        let url = Bundle(for: type(of: self)).url(forResource: "getAllResponse", withExtension: "json")!
+        let data = try! Data(contentsOf: url)
+        let getAllUrl = EquipmentRepository.getAllUrl()
+        stub(http(.get, uri: getAllUrl), jsonData(data))
+    }
+
+    func testExample() {
+        EquipmentRepository.getEquipment { (error) in
+            if let error = error {
+                XCTFail(error)
+            }
+            let equipment = EquipmentRepository.getEquipmentFromDatabase()
+            XCTAssert(!(equipment?.isEmpty ?? true)) //Should not be empty
+        }
+    }
+}
