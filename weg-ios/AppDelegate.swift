3a45b884d7ea974a45391609728b3a5cd48cdc71
jamesjmtaylor <james.jm.taylor@gmail.com>
2019-03-09 12:50:06 -0800

    Abstracted out to separate storyboards

--- weg-ios/AppDelegate.swift
+++ weg-ios/AppDelegate.swift
@@ -16,7 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         UINavigationBar.appearance().barStyle = .blackOpaque
         FirebaseApp.configure()
-//        Fabric.sharedSDK().debug = true
         return true
     }
     func applicationWillTerminate(_ application: UIApplication) {
